4f61296be88297aa0d1e975506e00bfae31e720e
Anssi Kesänen <anssi.kesanen@gmail.com>
2021-09-13 22:27:58 +0300

    -

--- config.h
+++ config.h
@@ -33,8 +33,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Signal",   NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Emacs",    NULL,	      NULL,       1 << 2,       0,           -1 },
-	{ "Thunderbird",   NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Thunderbird",   NULL,       NULL,       1 << 7,       0,           -1 },
 };
 
 /* layout(s) */
